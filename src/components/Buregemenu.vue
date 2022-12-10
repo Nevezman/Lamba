a4d38240fff3975896875f2dd25d0aedc88aa2f1
Nevezman <nevezy4ii321@gmail.com>
2022-12-10 15:54:25 +0400

    build

---
+++ src/components/Buregemenu.vue
@@ -0,0 +1,59 @@
+<template lang="">
+    <div class="burger__menu">
+        <div class="bureger__menu_list">
+            <Push>
+        <img class="menu__logo_image" src="@/assets/SVG/Logo.svg" alt="Эмблема Ламбы">
+        <ul class="menu__burger">
+            <li class="menu__items">
+                    <a class="menu__link" href="#Info_scroll">О гонке</a>
+                </li>
+                <li class="menu__items">
+                    <a class="menu__link" href="#track__scroll">Трек</a>
+                </li>
+                <li class="menu__items">
+                    <a class="menu__link" href="#superEvo__scroll">Lamborghini Super trofeo</a>
+                </li>
+                <li class="menu__items">
+                    <a class="menu__link" href="#blog__scroll">Новости</a>
+                </li>
+                <li class="menu__items">
+                    <a class="menu__link" href="#buy-billets_scroll">Купить билеты</a>
+                </li>
+                <li class="menu__items">
+                    <a class="menu__link" href="#watch_video">Смотреть онлайн</a>
+                    
+                </li>
+                <li class="menu__items">
+                    <a class="menu__link" href="#contacts__scroll">Контакты</a>
+                </li>
+            </ul>
+        </Push>
+            </div>
+            
+    </div>
+</template>
+<script>
+import { Push} from 'vue3-burger-menu' 
+export default {
+    components: {
+        Push
+    }
+}
+</script>
+<style scoped>
+     
+      .menu__logo_image {
+        margin-left: 55px;
+     }
+     .menu__items {
+        padding-bottom: 10px;
+     }
+     .menu__burger {
+        display: block;
+     }
+    @media (min-width: 950px) {
+        .burger__menu {
+            display: none;
+        }
+    }
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="footer">
+    <h3 class="footer__title" id="contacts__scroll">Контакты</h3>
+    <div class="footer__content_up">
+        <div class="footer__content_info">
+          <ul>
+            <li>
+                <dl class="footer__content_position">
+                     <dt class="footer__content_subtitle">ОБЩИЕ</dt>
+                     <dd class="footer__content_text"><a href="tel:+351282405600">+(351) 282 405 600</a></dd>
+                     <dd class="footer__content_text"><a href="tel:+351282405697">+(351) 282 405 697</a></dd>
+                     <dd class="footer__content_text"><a href="mailto:info@autodromodoalgarve.com">info@autodromodoalgarve.com</a> </dd>
+                </dl>
+            </li>
+            <li>
+                <dl class="footer__content_position">
+                     <dt class="footer__content_subtitle" id="buy-billets_scroll">БИЛЕТЫ</dt>
+                     <dd class="footer__content_text"><a href="tel:+351282405600">+(351) 282 405 600</a></dd>
+                     <dd class="footer__content_text"><a href="mailto:tickets@autodromodoalgarve.com">tickets@autodromodoalgarve.com</a> </dd>
+                </dl>
+            </li>
+            <li>
+                <ul class="footer__content_social-list">
+                    <li><a href=#><img src="../assets/SVG/YOUTUBE.svg" alt="youtube"></a></li>
+                    <li><a href=#><img src="../assets/SVG/INST.svg" alt="instogram"></a></li>
+                    <li><a href=#><img src="../assets/SVG/FACEBOOK.svg" alt="facrbook"></a></li>
+                </ul>
+            </li>
+          </ul>
+        </div>
+        <div class="footer__content_map">
+            <dl>
+                <dt class="footer__content_subtitle">АДРЕС</dt>
+                <dd class="footer__content_text">Sitio do Escampadinho, Mexilhoeira Grande, 8500-148 Portimao, Portugal </dd>
+            </dl>
+            <GoogleMap api-key="" style="width: 454зч; height: 253px" :center="center" :zoom="14" :options="options">
+    
+             <Marker :options="{ position: center }" />
+  </GoogleMap>
+        </div>
+    </div>
+    <div class="footer__content_down">
+
+    </div>
+    </div>
+</template>
+<script>
+
+  import { defineComponent } from "vue";
+import { GoogleMap, Marker } from "vue3-google-map";
+
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: 37.23162773154195, lng: -8.628272372789242 };
+   
+       
+    return { center
+}
+  }
+});
+
+    
+ 
+</script>
+<style>
+.footer__content_up {
+    display: flex;
+    justify-content: space-between;
+}
+ .footer__content_info ul {
+    list-style-type: none;
+ }   
+ .footer__title{
+    margin-bottom: 50px;
+ }
+ .footer__content_subtitle{
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    padding-bottom: 20px;
+ }
+ .footer__content_text{
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15.5307px;
+    padding-bottom: 10px;
+ }
+  .footer__content_position{
+    padding-bottom: 50px;
+  }
+  .footer__content_social-list {
+    display: flex;
+    gap: 20px;
+  }
+  @media (max-width: 760px){
+    .footer__content_up {
+    display: block;
+    }
+    .footer__content_social-list {
+        margin-bottom: 30px;
+    }
+
+  }
+
+
+</style>
